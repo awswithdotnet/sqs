92840efa8de0c98e9d59a790b7f71d584426848d
Feeley <feeleyfire@gmail.com>
2020-12-08 17:21:44 -0500

    initial commit

---
+++ SQSConsumer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SQSConsumer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            SQSMessageConsumer sqSConsumer = new SQSMessageConsumer();
+                       
+            await sqSConsumer.Listen();           
+                       
+        }
+    }
+}
---
+++ SQSProducer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SQSProducer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           var message =  "New Message for " + DateTime.Now.ToString();
+
+           SQSMessageProducer sQSMessageProducer = new SQSMessageProducer();
+
+           await sQSMessageProducer.Send(message);
+
+           Console.WriteLine("Message Sent: " + message);
+        }
+
+    }
+}
